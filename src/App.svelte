393c37adcdc70ea7cc489f8c6ab59c949bb115ef
Erik Larsson <erik.larsson@schibsted.com>
2020-03-29 22:21:21 +0200

    Blind copy paste PWA

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,10 @@
 <script>
-  import { onMount } from "svelte";
-  export let date;
-
-  onMount(async () => {
-    const res = await fetch("/api/date");
-    const newDate = await res.text();
-    date = newDate;
-  });
-
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker.js');
   }
 </script>
 
 <main>
-  <h1>Hello 20:01</h1>
-  <br />
-  <h2>The date according to Node.js is:</h2>
-  <p>{date ? date : 'Loading date...'}</p>
+  <h1>Hello</h1>
+  <p>{new Date()}</p>
 </main>

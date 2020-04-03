f769f3a1df17809826b6bc353cd491d6dfb1499d
Erik Larsson <erik.larsson@schibsted.com>
2020-04-03 09:59:32 +0200

    menu

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,24 @@
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker.js');
   }
+  import { onMount } from 'svelte';
   import '@material/mwc-list/mwc-list.js';
   import '@material/mwc-list/mwc-list-item.js';
+  import '@material/mwc-menu';
   import '@material/mwc-icon';
   import '@material/mwc-icon-button';
   import '@material/mwc-top-app-bar';
   import Teaser from './Teaser.svelte';
+
+  onMount(async () => {
+    // anchor must share a parent with menu that is `position: relative`
+    menu.anchor = menuButton;
+
+    menuButton.addEventListener('click', function (e) {
+      menu.open = true;
+      // alternatively you can use menu.show();
+    });
+  });
 </script>
 <style>
 mwc-top-app-bar {
@@ -40,14 +52,29 @@ img {
 }
 </style>
 <mwc-top-app-bar>
-    <mwc-icon-button icon="menu" slot="navigationIcon"></mwc-icon-button>
-    <div slot="title">All the things {version}</div>
-    <mwc-icon-button icon="favorite" slot="actionItems"></mwc-icon-button>
-    <div>
-<mwc-list>
-  {#each data as thing}
-    <Teaser {thing} />
-  {/each}
-</mwc-list>
-    </div>
+  <mwc-icon-button id="menuButton" icon="menu" slot="navigationIcon"></mwc-icon-button>
+    <mwc-menu id="menu">
+      <mwc-list-item graphic="avatar" twoline=1>
+        <span>All the things</span>
+        <span slot="secondary">{version}</span>
+        <img src="favicon.png" slot="graphic" />
+      </mwc-list-item>
+      <li divider role="separator"></li>
+      <mwc-list-item graphic="icon">
+        <slot>FAQ</slot>
+        <mwc-icon slot="graphic">help_outline</mwc-icon>
+      </mwc-list-item>
+      <mwc-list-item graphic="icon">
+        <slot>Sign out</slot>
+        <mwc-icon slot="graphic">exit_to_app</mwc-icon>
+      </mwc-list-item>
+    </mwc-menu>
+  <div slot="title">All the things</div>
+  <div>
+    <mwc-list>
+      {#each data as thing}
+        <Teaser {thing} />
+      {/each}
+    </mwc-list>
+  </div>
 </mwc-top-app-bar>

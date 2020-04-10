855a5e1b0745886efc288afc7f661e7aa775d9f2
Erik Larsson <erik.larsson@schibsted.com>
2020-04-10 14:32:54 +0200

    component poc

--- src/components/View.svelte
+++ src/components/View.svelte
@@ -1,9 +1,16 @@
 <script>
+  /* TODO
+  * Lists
+  * Sub-objects (how deep? how to tell sub-objects to render small?)
+  * Pretty
+  */
+
   import '@material/mwc-list/mwc-list.js';
   import '@material/mwc-list/mwc-list-item.js';
   import jsonld from 'jsonld';
   import recipeSchema from '../schemas/Recipe.json';
   import schemaContext from '../schemas/schemaContext.json';
+  import Duration from './view/Duration.svelte';
 
   const CONTEXTS = {
     "http://schema.org/": schemaContext,
@@ -24,22 +31,72 @@
   };
 
   const arrayMe = (n) => Array.isArray(n) ? n : [n];
-  export let thing;
+  const rangeIncludes = (p, t) => {
+    console.log(arrayMe(p["schema:rangeIncludes"]));
+    return arrayMe(p["schema:rangeIncludes"]).map(o => o["@id"]).includes(t);
+  }
+  export let thing, property;
 </script>
 
+{#if property}
+  {#if typeof(thing) !== "object"}
+    {#if rangeIncludes(property, "schema:Duration")}
+      <Duration {thing} {property} />
+    {:else}
+      <mwc-list-item twoline=1>
+        <span>{thing}</span>
+        <span slot="secondary">
+          {property["rdfs:label"]}. {property["rdfs:comment"]}
+        </span>
+      </mwc-list-item>
+    {/if}
+  {:else if Array.isArray(thing)}
+    <li divider role="separator"></li>
+      {#each thing as item}
+        <svelte:self thing={item} property={property} />
+      {/each}
+    <li divider role="separator"></li>
+  {:else}
+    {#if thing["@type"] === "schema:Date"}
+      <mwc-list-item twoline=1>
+        <span>{new Date(thing["@value"])}</span>
+        <span slot="secondary">
+          {property["rdfs:label"]}. {property["rdfs:comment"]}
+        </span>
+      </mwc-list-item>
+    {:else if !thing["@type"] && property["@id"] === "schema:image"}
+      <mwc-list-item graphic="large" twoline=1>
+        <img slot="graphic" src={thing["@id"]} alt={property["rdfs:label"]} />
+        <span>{property["rdfs:label"]}</span>
+        <span slot="secondary">
+          {property["rdfs:label"]}. {property["rdfs:comment"]}
+        </span>
+      </mwc-list-item>
+    {:else if !thing["@type"] && rangeIncludes(property, "schema:URL")}
+      <mwc-list-item twoline=1>
+        <span>{thing["@id"]}</span>
+        <span slot="secondary">
+          {property["rdfs:label"]}. {property["rdfs:comment"]}
+        </span>
+      </mwc-list-item>
+    {:else}
+      <mwc-list-item twoline=1>
+        <span>{console.log(property, thing) || property["rdfs:label"]} <strong>{thing["@type"]}</strong></span>
+        <span slot="secondary">
+          {#each arrayMe(property["schema:rangeIncludes"]).map(p => p["@id"]) as range}
+            &gt;{range}
+          {/each}
+          {property["rdfs:comment"]}
+        </span>
+      </mwc-list-item>
+    {/if}
+  {/if}
+{:else}
 {#await jsonld.compact(thing, { "@context": { "schema": "http://schema.org/" }}) then compact}
   <mwc-list>
     {#each recipeSchema['@graph'].filter(n => n["@type"] == "rdf:Property") as property}
       {#if compact[property["@id"]]}
-        <mwc-list-item twoline=1>
-          <span>{property["rdfs:label"]} <strong>{compact[property["@id"]]}</strong></span>
-          <span slot="secondary">
-            {#each arrayMe(property["schema:rangeIncludes"]).map(p => p["@id"]) as range}
-              &gt;{range}
-            {/each}
-            {property["rdfs:comment"]}
-          </span>
-        </mwc-list-item>
+        <svelte:self thing={compact[property["@id"]]} property={property} />
       {/if}
     {/each}
   </mwc-list>
@@ -47,3 +104,4 @@
     {console.log(compact) || ""}
   </pre>
 {/await}
+{/if}
---
+++ src/components/view/Duration.svelte
@@ -0,0 +1,23 @@
+<script>
+  import '@material/mwc-list/mwc-list-item.js';
+  import { parse } from 'tinyduration';
+  export let thing;
+  export let property;
+  const humanReadableDuration = (iso8601) => {
+    const { years, months, weeks, days, hours, minutes, seconds } = parse(iso8601);
+    return (years ? `${years}y ` : '') +
+      (months ? `${months}mo ` : '') +
+      (weeks ? `${weeks}w ` : '') +
+      (days ? `${days}d ` : '') +
+      (hours ? `${hours}h ` : '') +
+      (minutes ? `${minutes}min ` : '') +
+      (seconds ? `${seconds}s ` : '');
+  };
+</script>
+
+<mwc-list-item twoline=1>
+  <span>{humanReadableDuration(thing)}</span>
+  <span slot="secondary">
+    <i>{property["rdfs:label"]}</i> {property["rdfs:comment"]}
+  </span>
+</mwc-list-item>
